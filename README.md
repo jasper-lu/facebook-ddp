465929349b40b734d129ef3c1c20f94aaae7a876
Jasper Lu <repsajrules@gmail.com>
2015-01-20 22:15:42 -0500

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+#Facebook-ddp
+
+Modification of meteor core package facebook so that mobile clients can log in with oauth.
+
+Add through the accounts-facebook-ddp package by using:
+
+    meteor add jasperlu:accounts-facebook-ddp
+
+This pair of packages is needed to implement login with OAuth for mobile ddp clients.
---
+++ package.js
@@ -0,0 +1,31 @@
+Package.describe({
+  name: 'jasperlu:facebook-ddp',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: "Facebook OAuth flow to include mobile ddp clients",
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/jasper-lu/facebook-ddp',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.0');
+  api.use('oauth2', ['client', 'server']);
+  api.use('oauth', ['client', 'server']);
+  api.use('http', ['server']);
+  api.use('templating', 'client');
+  api.use('underscore', 'server');
+  api.use('random', 'client');
+  api.use('service-configuration', ['client', 'server']);
+
+  api.export('Facebook');
+
+  api.addFiles(
+    ['facebook_configure.html', 'facebook_configure.js'],
+    'client');
+
+  api.addFiles('facebook_server.js', 'server');
+  api.addFiles('facebook_client.js', 'client');
+});
